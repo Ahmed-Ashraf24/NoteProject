adcadf4bad2680cd5a2bea8644244b3930e7c071
Ahmed Ashraf <ahmedashrafahmed2716@gmail.com>
2025-10-16 17:17:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/noteproject/ui/theme/Color.kt
@@ -0,0 +1,24 @@
+package com.example.noteproject.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+val customButtonColor = Color(0xFF3B3B3B)
+val backGroundColor = Color(0xFF252525)
+val whiteColor = Color(0xFFCCCCCC)
+val contentTitleColor = Color(0xFF9A9A9A)
+val greenButtonColor = Color(0xFF30BE71)
+val noteColorList = listOf(
+    Color(0xFFFD99FF),
+    Color(0xFFFF9E9E),
+    Color(0xFF91F48F),
+    Color(0xFF30BE71),
+    Color(0xFF9EFFFF),
+    Color(0xFFB69CFF)
+)
